165a2e3baf413f1cd5ac3df7ece8233b5fc1a5c0
Fernando Rodrigues <nandorodpires@gmail.com>
2022-03-14 11:46:22 -0300

    first commit

---
+++ src/Hello.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Stylesheets\Movidesk;
+
+class Hello
+{
+    public function say()
+    {
+        echo 'Hello';
+    }
+}
---
+++ test.php
@@ -0,0 +1,8 @@
+<?php
+
+use Stylesheets\Movidesk\Hello;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$hello = new Hello;
+$hello->say();
